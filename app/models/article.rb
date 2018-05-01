6b0fc5a80fffa2286a3fa096982008ab4c32f11b
Vrinda Gupta <vrindag@andrew.cmu.edu>
2018-04-30 20:08:54 -0400

    yay

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ApplicationRecord
+	has_many :categories
+
+	validates_presence_of :title
+	validates_presence_of :content
+
+	scope :alphabetical, -> { order('title') }
+	scope :active, -> { where(active: true)}
+
+end
